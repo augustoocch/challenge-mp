1d194eeb5dcbe5b2f0e0efcbd89896b17c87325e
augustoocch <augusto.occh@gmail.com>
2024-09-28 08:42:57 -0300

    Creation of statistics service

---
+++ statistics-service/src/test/java/com/meli/challenge/statisticts/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.challenge.statisticts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
